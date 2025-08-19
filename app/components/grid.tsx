f1e7ea77448074105ea914e8d7ac31eb012aebb8
Leprekus <leprekus@yahoo.com>
2025-08-19 13:16:25 -0700

    create colliding vertices prototype

--- app/components/grid.tsx
+++ app/components/grid.tsx
@@ -29,12 +29,36 @@ export default function Grid() {
 			d3.zoom<SVGSVGElement, unknown>()
 				.scaleExtent([0.15, 4.0])
 				.on('zoom', event => {
-				console.log('transform', String(event.transform));
-				setTransform(String(event.transform))
+					setTransform(String(event.transform))
 				})
 		);
-	});
-	console.log(vertexCount, Array(vertexCount).length);
+
+
+		const datums = Array.from({ length: 69 }, () => ({}));
+		
+		const sim = d3.forceSimulation(datums)
+			.force('x', d3.forceX(200).strength(0.05))
+			.force('y', d3.forceY(200).strength(0.05))
+			.force('collide', d3.forceCollide(24))
+
+		const vertices = svg	
+			.selectAll('circle')
+			.data(datums)
+			.enter()
+			.append('circle')
+			.attr('r', 10)
+			.attr('fill', 'blue')
+			.attr('stroke', 'yellow')
+			.attr('style', 'transition-property: fill; transition-duration: 200ms;');
+		sim.alphaDecay(0);
+		sim.on('tick', () => {
+			vertices
+				.attr('cx', d => d.x)
+				.attr('cy', d => d.y)
+		})
+
+	}, []);
+
   return (
 	<svg 
 		className='bg-red-500 size-96 relative  m-auto' 
