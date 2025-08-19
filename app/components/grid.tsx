3f6c1a96657ea1c6728c7e5fb3924f082342c36f
Leprekus <leprekus@yahoo.com>
2025-08-19 14:42:19 -0700

    begin implementation for dragagble vertices

--- app/components/grid.tsx
+++ app/components/grid.tsx
@@ -1,73 +1,54 @@
 'use client'
 import * as d3 from 'd3';
-import { cloneElement, ComponentProps, ReactElement, ReactNode, useEffect, useRef, useState } from "react";
+import { cloneElement, ComponentProps, DragEventHandler, MouseEventHandler, ReactElement, ReactNode, useEffect, useRef, useState } from "react";
 
-interface VertexProps { transform : string };
-function Vertex ({ transform }: VertexProps) {
+enum EventTypes {
+	MouseDown = 'mousedown',
+	MouseUp = 'mouseup', 
+	TouchStart = 'touchstart',
+	TouchEnd = 'touchend'
+
+};
+function Vertex () {
+	const [transform, setTransform] = useState<[number, number, number]>([0, 0, 0]);
+	const handleDrag = (event: React.MouseEvent | React.TouchEvent) => {
+		event.stopPropagation();
+		event.nativeEvent.stopImmediatePropagation();
+		event.nativeEvent.stopPropagation();
+		event.preventDefault();
+		if (event.type === EventTypes.MouseDown)
+			console.log(EventTypes.MouseDown)
+		if (event.type === EventTypes.MouseUp)
+			console.log(EventTypes.MouseUp)
+		if (event.type === EventTypes.TouchStart)
+			console.log(EventTypes.TouchStart)
+		if (event.type === EventTypes.TouchEnd)
+			console.log(EventTypes.TouchEnd)
+	};
 	return (
-		<circle
-			r={10}
-			stroke='black'
-			fill='white'
-			className='transition-colors duration-200 hover:fill-gray-200'
-			transform={transform}
+		<div
+			className='size-10 bg-blue-500'
+			onClick={handleDrag} // stop the propagation to parent
+			onMouseDown={handleDrag}	
+			onMouseUp={ handleDrag}
+			onTouchStart={handleDrag}
+			onTouchEnd={handleDrag}
 		/>
 	);
 }
 //type Selection<T extends d3.BaseType> = d3.Selection<T, unknown, null, undefined>;
 type VertexComponent = ReactElement<ComponentProps<typeof Vertex>>;
 export default function Grid() {
-	const svgRef = useRef<SVGSVGElement | null>(null);
 	const [vertexCount, setVertexCount] = useState<number>(0);
-	const [transform, setTransform] = useState<string>('');
 	useEffect(() => {
-		if(!svgRef.current) return;
-		svgRef.current.onclick = 
-			() => void setVertexCount(prev => prev + 1);
-		const svg = d3.select(svgRef.current);
-		svg.call(
-			d3.zoom<SVGSVGElement, unknown>()
-				.scaleExtent([0.15, 4.0])
-				.on('zoom', event => {
-					setTransform(String(event.transform))
-				})
-		);
-
-
-		const datums = Array.from({ length: 69 }, () => ({}));
-		
-		const sim = d3.forceSimulation(datums)
-			.force('x', d3.forceX(200).strength(0.05))
-			.force('y', d3.forceY(200).strength(0.05))
-			.force('collide', d3.forceCollide(24))
-
-		const vertices = svg	
-			.selectAll('circle')
-			.data(datums)
-			.enter()
-			.append('circle')
-			.attr('r', 10)
-			.attr('fill', 'blue')
-			.attr('stroke', 'yellow')
-			.attr('style', 'transition-property: fill; transition-duration: 200ms;');
-		sim.alphaDecay(0);
-		sim.on('tick', () => {
-			vertices
-				.attr('cx', d => d.x)
-				.attr('cy', d => d.y)
-		})
-
 	}, []);
 
   return (
-	<svg 
+	<div 
 		className='bg-red-500 size-96 relative  m-auto' 
-		ref={svgRef}
+		onMouseDown={() => setVertexCount(prev => prev + 1)}
 	>
-		{Array(vertexCount)
-			.fill(0)
-			.map((_, i) => <Vertex transform={transform} key={`vertex-${i}`}/>)
-		}
-	</svg>
+		{Array.from({ length: vertexCount }, (_, i) => <Vertex key={i}/>)}
+	</div>
   );
 }
