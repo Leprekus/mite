464314166fdf69ba8839c2c4ebbffe51664bc1b9
Leprekus <leprekus@yahoo.com>
2025-08-20 23:13:47 -0700

    refactor and use graph-controller class

--- app/components/grid.tsx
+++ app/components/grid.tsx
@@ -1,4 +1,5 @@
 'use client'
+import GraphController, { LinkDatum, Vertex } from '@/lib/graph-controller';
 import * as d3 from 'd3';
 import { cloneElement, ComponentProps, DragEventHandler, MouseEventHandler, ReactElement, ReactNode, useEffect, useRef, useState } from "react";
 
@@ -13,98 +14,33 @@ enum EventTypes {
 };
 
 //type Selection<T extends d3.BaseType> = d3.Selection<T, unknown, null, undefined>;
-type Vertex = {
-	x: number;
-	y: number;
-	r: number;
-	fx?: number | null;
-	fy?: number | null;
-} | undefined;
+
 export default function Grid() {
 	const canvasRef = useRef<HTMLCanvasElement| null>(null);
 	useEffect(() => {
 		if(!canvasRef.current) return;
-		const width = Math.min(window.innerWidth, window.innerHeight),
-		      height = width;
-		const radius = 20;
-		let Nodes = Array.from({ length: 60 }, (_, i) => ({
+		let nodes: Vertex[] = Array.from({ length: 60 }, (_, i) => ({
 			id: i,
 			x: (Math.random() * 2) - 1,
 			y: (Math.random() * 2) - 1,
-			r: radius
+			vx: 0,
+			vy: 0,
+			fx: null,
+			fy: null,
 		}));
-		const Links = Nodes.map((_, i) => ({
+		const links: LinkDatum[] = nodes.map((_, i) => ({
 			source: i, 
-			target: Math.min(i + 1, Nodes.length - 1)
+			target: Math.min(i + 1, nodes.length - 1)
 		}));
-
-		const nodes = Nodes.map(n => Object.create(n));
-		const links = Links.map(n => Object.create(n));
-		console.log(Nodes, nodes);
-
 		const canvas = canvasRef.current;
-		canvas.width = width;
-		canvas.height = height;
-		const context = canvas.getContext('2d');
-		if(!context) throw Error('no context');
-
-		const simulation = d3.forceSimulation(nodes)
-			.force('charge', d3.forceManyBody().strength(-30))
-			.force('link', d3.forceLink(links).strength(1).distance(radius * 4).iterations(10))
-			.on('tick', () => {
-				context.clearRect(0, 0, width, height);
-				context.save();
-				context.translate(width / 2, height / 2);
-				context.beginPath();
-				for(const d of links) {
-					context.moveTo(d.source.x, d.source.y);
-					context.lineTo(d.target.x, d.target.y);
-				}
-				context.strokeStyle = '#aaa';
-				context.stroke();
-				context.beginPath();
-				for(const d of nodes) {
-					context.moveTo(d.x + radius, d.y);
-					context.arc(d.x, d.y, radius, 0, 2 * Math.PI);
-				}
-				context.fill();
-				context.strokeStyle = '#fff';
-				context.stroke();
-				context.restore();
-			})
-			
-		const drag = d3.drag<HTMLCanvasElement, unknown, Vertex>()
-			.subject(({x, y}) => simulation.find(x - width / 2, y - height / 2, 40))
-			.on('start', event => {
-				//ensures each event starts from an initial 'restart' state
-				//otherwise it freezes and prevents subsequent 'start' events from running
-				if(!event.active) simulation.alphaTarget(0.3).restart();
-				event.subject.fx = event.subject.x;
-				event.subject.fy = event.subject.y;
-			})
-			.on('drag', event => {
-				//update the (x, y) positions
-				event.subject.fx = event.x;
-				event.subject.fy = event.y;
-			})
-			.on('end', event => {
-				if(!event.active) simulation.alphaTarget(0); //i think this saves the simulation in the 'end' state.
-				event.subject.fx = null;
-				event.subject.fy = null;
-			});
-
-			d3.select(context.canvas).call(drag).node();
-			const linkForce = simulation.force('link');
-			console.log({ linkForce })
-			if(!linkForce) 
-				throw new Error('linkForce is undefined');
-			linkForce!.initialize(nodes, () => Math.random());
-			canvas.onclick = () => {
-				const popped = simulation.nodes().pop();
-			}
+		const gc = new GraphController(
+			canvas,
+			nodes,
+			links
+		);
+		gc.render();
 			return () => {
-				if(!canvas) return;
-				canvas.onclick = null;
+				gc.destroy();
 			};
 			
 	}, []);
