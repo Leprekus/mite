8c1ee70c44153b9644ea0a36133b98581cdde353
Leprekus <leprekus@yahoo.com>
2025-08-21 21:07:05 -0700

    create button component

---
+++ components/ui/button.tsx
@@ -0,0 +1,28 @@
+import { forwardRef } from "react";
+import { cn } from '@/lib/utils';
+export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  // add any custom props here
+}
+
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ children, className = '', ...props }, ref) => {
+    return (
+      <button 
+		ref={ref} {...props}
+		className={cn(`px-4 py-2 rounded-lg border border-gray-300 bg-white text-gray-800 
+                   hover:bg-gray-100 active:bg-gray-200 focus:outline-none 
+                   focus:ring-2 focus:ring-gray-400 focus:ring-offset-2
+                   transition-colors duration-150 font-medium `, className)}
+	
+		   {...props}
+	>
+        {children}
+      </button>
+    );
+  }
+);
+
+Button.displayName = "Button";
+
+export default Button;
+
