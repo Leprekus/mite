2f115f146391975362f198138530a055d30f4510
Leprekus <leprekus@yahoo.com>
2025-08-21 22:14:21 -0700

    implement kruskal

--- lib/graph.ts
+++ lib/graph.ts
@@ -1,15 +1,35 @@
 import { LinkDatum, Vertex } from "./graph-controller";
+import UnionFind from "./union-find";
 
-type Edge = LinkDatum;
+interface Edge extends LinkDatum {
+    id: string;
+};
 class Graph {
-    vertices: Vertex[]; 
-    edges: Edge[];
-    private unionFind() {}
-    constructor(vertices: Vertex[], edges: Edge[]) {
-        this.vertices = vertices;
-        this.edges = edges;
+    private vertices: Vertex[]; 
+    private edges: Edge[];
+    constructor(vertices: Vertex[], edges: LinkDatum[]) {
+        this.vertices = vertices.map(v => ({...v}));
+        this.edges = edges.map(e => ({
+            ...e, 
+            id: (e.source as Vertex).id 
+        }));
     }
-    kruskal() {}
+    kruskal() {
+        const uf = new UnionFind<Vertex>();
+        const tree: Edge[] = [];
+        this.vertices.map(v => uf.add(v));
+        const edges = this.edges
+            .sort((a, b) => a.weight - b.weight);
+        edges.map(e => {
+            const from = e.source as Vertex;
+            const to = e.target as Vertex;
+            if(uf.find(from).parent !== uf.find(to).parent) {
+                uf.union(from, to);
+                tree.push(e);
+            }
+        });
+        return tree;
+    }
     prim() {}
     dijkstra() {}
     bellmanFord() {}
